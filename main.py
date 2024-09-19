98a86fa5dbe429fbca88e9730fc629438c9982e6
alexboehne <alex.boehne04@gmail.com>
2024-09-19 17:51:15 -0500

    - Added incorrect password limit, results in 2 minute timeout

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import PySimpleGUI as pg
 import pyotp
 import qrcode
@@ -23,6 +24,8 @@ def keygen(key_input):
     return key # returns 16-character key string
 
 def main():
+    incorrect_guesses = 0 # incorrect guesses counter
+    timeout_end = " "
 
     window_layout = [ # sets layout of elements in window
         [pg.Text("Please enter your password:",
@@ -51,38 +54,50 @@ def main():
             break
 
         if event == '-SUBMIT-': # on submit button click
-            check = values['-INPUT-'].encode('utf-8') # get byte string from input box
+            if incorrect_guesses < 3: # brute force protection, only evaluates if incorrect guesses is less than 3
+                check = values['-INPUT-'].encode('utf-8') # get byte string from input box
 
-            # if you want to manually input a password into the secrets file, you
-            # can comment out the line below and copy the output and skim the "b'" and "'"
-            # from the beginning and end respectively
+                # if you want to manually input a password into the secrets file, you
+                # can comment out the line below and copy the output and skim the "b'" and "'"
+                # from the beginning and end respectively
 
-            # print(get_hashed_password(check))
+                # print(get_hashed_password(check))
 
-            # read hashed password from file
-            with open("secrets.txt", "r") as file: # open file, iterate through lines
-                for line in file:
-                    new_check = line.strip().encode('utf-8') # strips/encodes password
-                    try:
-                        if check_password(check, new_check): # check if password matches stored hashes
-                            window['-OUTPUT-'].update(" ") # clears error output label
-                            test_otp = pyotp.totp.TOTP(keygen(values['-INPUT-'])) # gets auth code object
+                # read hashed password from file
+                with open("secrets.txt", "r") as file: # open file, iterate through lines
+                    for line in file:
+                        new_check = line.strip().encode('utf-8') # strips/encodes password
+                        try:
+                            if check_password(check, new_check): # check if password matches stored hashes
+                                window['-OUTPUT-'].update(" ") # clears error output label
+                                test_otp = pyotp.totp.TOTP(keygen(values['-INPUT-'])) # gets auth code object
 
-                            # For testing if OTP code associated with password is correct
-                            # print(test_otp.now())
+                                # For testing if OTP code associated with password is correct
+                                # print(test_otp.now())
 
-                            code = pg.popup_get_text("Enter your 2FA authenticator code") # popup prompt
+                                code = pg.popup_get_text("Enter your 2FA authenticator code") # popup prompt
 
-                            if int(code) == int(test_otp.now()):
-                                pg.popup_ok("Authentication successful! :D")
-                            else:
-                                pg.popup_ok("Authentication failed - please try again")
-                            break
-                        else: # incorrect password
-                            window['-OUTPUT-'].update("Incorrect password!")
+                                if int(code) == int(test_otp.now()):
+                                    pg.popup_ok("Authentication successful! :D")
+                                else:
+                                    pg.popup_ok("Authentication failed - please try again")
+                                break
+                            else: # incorrect password
+                                window['-OUTPUT-'].update("Incorrect password!")
+                                incorrect_guesses += 1
 
-                    except ValueError: # sometimes throws if salt gets freaky, usually incorrect password
-                        pass
+                        except ValueError: # sometimes throws if salt gets freaky, usually incorrect password
+                            pass
+            else: # if you have exceeded 3 incorrect guesses
+                pg.popup_annoying("You have exceeded the maximum number of attempts. Please wait 2 minutes for new attempts to login.")
+                if timeout_end == " ": # set timer if not already set
+                    timeout_end = datetime.datetime.now() + datetime.timedelta(minutes=2) # sets time to +2 min from now
+
+                if datetime.datetime.now() > timeout_end: # if timer is up
+                    timeout_end = " " # set timer to nothing
+                    incorrect_guesses = 0 # reset incorrect guesses
+                else: # if timer is not up
+                    window['-OUTPUT-'].update("Please wait before entering more passwords")
 
         # qr code generator
         if event == '-GEN-QR-CODE-':
